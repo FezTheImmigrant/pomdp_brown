530de64b6c5b4d43e5f918cdb3199704399ad53f
Sergio Orozco <sergio.orozco@yorkspacesystems.com>
2022-06-19 10:58:07 -0600

    format files

--- parameters_component.py
+++ parameters_component.py
@@ -1,5 +1,6 @@
 import json
 
+
 class ParametersComponent:
     Walls = []
     EndStates = []
@@ -7,32 +8,33 @@ class ParametersComponent:
     Col = 0
     Reward = 0
     Lambda = 0
-    def load_parameters(self,file_name):
 
-        f= open (file_name,"r")
+    def load_parameters(self, file_name):
+
+        f = open(file_name, "r")
 
         data = json.loads(f.read())
 
-        if self.is_positive_int(data['Rows']):
-            self.Row = int(data['Rows'])
+        if self.is_positive_int(data["Rows"]):
+            self.Row = int(data["Rows"])
         else:
             print("Row parameter must be a postive integer value.")
             return False
 
-        if self.is_positive_int(data['Columns']):
-            self.Col = int(data['Columns'])
+        if self.is_positive_int(data["Columns"]):
+            self.Col = int(data["Columns"])
         else:
             print("Column parameter must be a positive integer value.")
             return False
 
-        if self.is_float(data['Reward']):
-            self.Reward = float(data['Reward'])
+        if self.is_float(data["Reward"]):
+            self.Reward = float(data["Reward"])
         else:
             print("Reward parameter must be a float value.")
             return False
 
-        if self.is_float(data['Lambda']):
-            self.Lambda = float(data['Lambda'])
+        if self.is_float(data["Lambda"]):
+            self.Lambda = float(data["Lambda"])
         else:
             print("Lambda parameter must be a float value.")
             return False
@@ -40,46 +42,49 @@ class ParametersComponent:
         for wall in data["Walls"]:
 
             if not len(wall) == 2:
-                print ("A wall takes only two parameters: (row,column)")
+                print("A wall takes only two parameters: (row,column)")
                 return False
 
             if not self.is_positive_int(wall[0]) or not self.is_positive_int(wall[1]):
-                print ("A wall takes only positive integer parameters")
+                print("A wall takes only positive integer parameters")
                 return False
 
             self.Walls.append(wall)
 
         for end_state in data["EndStates"]:
 
-
             if not len(end_state) == 3:
-                print ("An end state takes only three parameters: (row,column,utility)")
+                print("An end state takes only three parameters: (row,column,utility)")
                 return False
 
-            if not self.is_positive_int(end_state[0]) or not self.is_positive_int(end_state[1]):
-                print ("The first two parameters for an end state must be positive integer parameters")
+            if not self.is_positive_int(end_state[0]) or not self.is_positive_int(
+                end_state[1]
+            ):
+                print(
+                    "The first two parameters for an end state must be positive integer parameters"
+                )
                 return False
             if not self.is_float(end_state[2]):
-                print ("The third parameter for an end state must be a float.")
-            
+                print("The third parameter for an end state must be a float.")
+
             self.EndStates.append(end_state)
 
         return True
 
-    def is_positive_int(self,inp):
+    def is_positive_int(self, inp):
         is_positive_int = False
         try:
-            int (inp)
+            int(inp)
             is_positive_int = int(inp) >= 0
         except ValueError:
             is_positive_int = False
 
         return is_positive_int
 
-    def is_float(self,inp):
+    def is_float(self, inp):
         is_float = False
         try:
-            float (inp)
+            float(inp)
             is_float = True
         except ValueError:
             is_float = False
